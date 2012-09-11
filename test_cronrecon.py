1e1b2980cf2a0d90ddf3ebc3dcd5120adc4fe1ad
Harvey <harveyr@gmail.com>
2012-09-11 06:45:27 -0700

    added two rollover unit tests

--- test_cronrecon.py
+++ test_cronrecon.py
@@ -1,4 +1,5 @@
 import logging
+import datetime
 from cronrecon import CronJob
 
 
@@ -62,3 +63,55 @@ def test_dow2():
     assert next_run.hour >= 9 and next_run.hour <= 12
     assert next_run.weekday() == dow
     assert next_run.month == 1 or next_run.month % 4 - 1 == 0
+
+
+def test_month_rollover():
+    minute = '*'
+    hour = '*'
+    dom = '3-5,9-22'
+    month = '*/4'
+    dow = '*'
+
+    cron_str = '{0} {1} {2} {3} {4}'.format(
+        minute, hour, dom, month, dow)
+    job = CronJob(cron_str)
+
+    start_dt = datetime.datetime(
+        year=2012,
+        month=2,
+        day=27)
+
+    next_run = job.next_run(start_dt)
+    assert next_run.month == 5
+    assert next_run.day == 3
+
+
+def test_day_and_hour_rollover():
+    minute = '*'
+    hour = '12,19'
+    dom = '3,22'
+    month = '*'
+    dow = '*'
+
+    cron_str = '{} {} {} {} {}'.format(
+        minute, hour, dom, month, dow)
+    job = CronJob(cron_str)
+
+    start_dt = datetime.datetime(
+        year=2012,
+        month=4,
+        day=1)
+
+    next_run = job.next_run(start_dt)
+    assert next_run.day == 3
+    assert next_run.hour == 12
+
+    start_dt = next_run + datetime.timedelta(hours=1)
+    next_run = job.next_run(start_dt)
+    assert next_run.hour == 19
+    assert next_run.day == 3
+
+    start_dt = next_run + datetime.timedelta(hours=1)
+    next_run = job.next_run(start_dt)
+    assert next_run.day == 22
+    assert next_run.hour == 12
